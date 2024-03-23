67e1f70e7d497f1e1a377cb26151ff863a80d2eb
05Rohit <25rohitkuamr98@gmail.com>
2024-03-23 13:41:09 +0530

    Apply filter method more accurate

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-const ProductCard = ({ images,title,actor,dor,year,poster,genre,director,rated,runtime}) => {
-
-
-
+const ProductCard = ({
+  images,
+  title,
+  actor,
+  year,
+  runtime,
+  type,
+  language
+}) => {
   return (
     <div>
-    
-      <div className="bg-gray-800 min-w-[250px] max-w-[250px] h-[480px] shadow-lg justify-center items-center  px-3 mt-8 mb-8 border-2 border-gray-600  hover:shadow-2xl ">
+      <div className="bg-gray-800 min-w-[250px] max-w-[300px] h-[480px] shadow-lg justify-center items-center  px-3 mt-8 mb-8 border-2 border-gray-600  hover:shadow-2xl ">
         <div className="max-h-[180px]">
           <img
             src={images}
@@ -16,24 +20,23 @@ const ProductCard = ({ images,title,actor,dor,year,poster,genre,director,rated,r
           />
         </div>
 
-        <p className="p-1 mt-5  capitalize text-xl font-bold ">
-          {title}{" "}
-        </p>
-        <p className="p-1 mt-1  capitalize text-base ">
-          {actor}{" "}
-        </p>
-        <p className="p-1  capitalize text-base">
-          {year}{" "}
-        </p>
-        <p className="p-1  capitalize text-base ">
-          {rated}{" "}
-        </p>
-        <p className="p-1 capitalize text-base ">
-          {runtime}{" "}
-        </p>
-       
+        <div className="flex justify-between p-1 mt-5">
+          <div className="capitalize text-xl ">
+          <p className="   text-[#EDBF15] font-semibold  ">{title} </p>
+          <p className="text-sm  "> {language} </p>
+
+          </div>
+          
+          <p className=" h-min text-[18] capitalize border-l-2 border-[#EDBF15] pl-2 mr-3 ">{type} </p>
+        </div>
+        <div className="pl-1 mt-2">
+          <p>Actor's name : </p>
+          <p className=" capitalize text-base ">{actor} </p>
+        </div>
+
+        <p className="p-1 mt-2  capitalize text-base">Year: {year} </p>
+        <p className="p-1 capitalize text-base ">Duration: {runtime} </p>
       </div>
-    
     </div>
   );
 };
--- src/productlist.jsx
+++ src/productlist.jsx
@@ -4,40 +4,21 @@ import ProductCard from "./ProductCard";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
-  const [data,setData]=useState('')
-
+  const [searchData, setSearchData] = useState("");
 
   useEffect(() => {
     // Fetch data from the API
-    const res = fetch("https://gist.githubusercontent.com/saniyusuf/406b843afdfb9c6a86e25753fe2761f4/raw/523c324c7fcc36efab8224f9ebb7556c09b69a14/Film.JSON")
+    const res = fetch(
+      "https://gist.githubusercontent.com/saniyusuf/406b843afdfb9c6a86e25753fe2761f4/raw/523c324c7fcc36efab8224f9ebb7556c09b69a14/Film.JSON"
+    )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         setProducts(data);
-        
       })
       .catch((error) => console.error("Error fetching data:", error));
-    console.log(res);
+      console.log(res);
   }, []);
-  
-
-
-  function searchData(event) {
-    const searchValue = event.target.value;
-    setData(searchValue);
-    if(!searchValue)
-    {
-      setProducts(products)
-    }
-    else{
-      const filteredProducts = products.filter((item) => {
-        // Assuming item.Title contains the movie name
-        return item.Title.toLowerCase().includes(searchValue.toLowerCase());
-      });
-      setProducts(filteredProducts)
-
-    }
-  }
 
   return (
     <div className="app">
@@ -51,46 +32,45 @@ function ProductList() {
             <li className=" hover:text-cyan-500 cursor-pointer">
               Home <span className="sr-only">(current)</span>
             </li>
-
-            <li className=" hover:text-cyan-500 cursor-pointer">Contact Us</li>
-            <li className="hover:text-cyan-500 cursor-pointer">Registration</li>
             <li className="hover:text-cyan-500 cursor-pointer">Login</li>
           </ul>
         </div>
       </div>
 
       <div className="flex justify-center bg-black ">
-        <input type="search" value={data} placeholder="Search the movie name" className="rounded-sm border-none py-1 px-2 w-[600px]" onChange={searchData} />
+        <input
+          type="search"
+          value={searchData}
+          placeholder="Search the Movie "
+          className="rounded-md border-none py-1 px-5 w-[600px]"
+          onChange={(e) => setSearchData(e.target.value)}
+        />
       </div>
 
       <div className="right pt-5 flex flex-wrap gap-14 justify-center bg-black text-white ">
-
-     
-        {products &&
-          products.map((el) => {
-            return (
-              <ProductCard
-                key={el._id}
-                images={el.Images[0]}
-                title={el.Title}
-                actor={el.Actors}
-                dor={el.Released}
-                year={el.Year}
-                poster={el.Poster}
-                genre={el.Genre}
-                director={el.Director}
-                rated={el.Rated}
-                runtime={el.runtime}
-
-
-            
-               
-              />
-            );
-          })}
+        {products
+          .filter((el) => {
+            return searchData.toLowerCase() === ""
+              ? el : el.Title.toLowerCase().includes(searchData.toLowerCase());
+          })
+          .map((el) => (
+            <ProductCard
+              key={el._id}
+              images={el.Images[0]}
+              title={el.Title}
+              type={el.Type}
+              actor={el.Actors}
+              dor={el.Released}
+              year={el.Year}
+              poster={el.Poster}
+              genre={el.Genre}
+              director={el.Director}
+              rated={el.Rated}
+              runtime={el.Runtime}
+              language={el.Language}
+            />
+          ))}
       </div>
-
-     
     </div>
   );
 }
